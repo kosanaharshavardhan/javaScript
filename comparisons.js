c6b0510e3801ab79be61100fe5337751f40d4628
kosanaharshavardhan <kosana.harshavardhan0705@gmail.com>
2025-05-02 18:23:57 +0530

    add comparisions

---
+++ comparisons.js
@@ -0,0 +1,8 @@
+console.log("Welcome to Comparisions")
+console.log(null>0) // makes it 0
+console.log(null==0) // makes it undefined
+console.log(null>=0) //true 0>=0
+
+console.log(undefined==0)
+console.log(undefined>=0)
+console.log(undefined<0)
--- typeConversion.js
+++ typeConversion.js
@@ -3,3 +3,12 @@ let numstr=Number(str);
 console.log(typeof numstr)
 console.log(NaN===NaN)//use .isNan()
 // Number(null)=0;
+
+
+
+//
+console.log("1"+2);
+console.log("1"+2+2)
+console.log(1+1+"2")
+
+//+true --> a number conversion
