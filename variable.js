b27d97a4ec31ba53260c4cb8bf45fbbb34c05163
kosanaharshavardhan <kosana.harshavardhan0705@gmail.com>
2025-05-02 17:43:32 +0530

    add variables file

---
+++ variable.js
@@ -0,0 +1,8 @@
+var globalName;
+let localName;
+const unChangable="Hello";
+noDataType=1;
+console.log(typeof(noDataType));
+// var is a global scope avoid using it
+//----------------
+
---
+++ variable2.js
@@ -0,0 +1,19 @@
+"use strict"
+// treat all code into new version of js
+//alert() --> cannot be worked in node js environment
+
+
+//for notes
+// tc39 && mdn
+
+//datatypes
+let a=1;
+let b=2222222222222222222222;
+let c="a";
+let d=Symbol('a');
+let e=true;
+let f=null; //NULL 
+let g//undefined
+console.log(typeof(d))
+console.log(typeof(f))
+console.log(typeof(g))
