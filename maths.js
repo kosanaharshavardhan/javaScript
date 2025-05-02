ce95b9093fb51f3c901f4743285bdbc1274d9163
kosanaharshavardhan <kosana.harshavardhan0705@gmail.com>
2025-05-02 19:36:34 +0530

    add maths and number functions

---
+++ maths.js
@@ -0,0 +1,23 @@
+const num=new Number(100);
+// a number object;
+console.log(num)
+// number methods:
+/*
+ 1. toString() --> datatype conversion
+ 2. toFixed() --> fixed decimals
+ 3. to Precision() --> fixed digits
+ gives exp if more digits were there
+ */
+// .toLocaleString('en-IN') -->currency commas.
+//tIP: USE DATATYPE() function in console brwoser window to know properties or length
+// or when you declare a variable using a constructor method you can also view using it
+//=-=-=-=--=-=-=++_+_+_+_+_+-=+-
+//math methods
+console.log(Math.ceil(0.4));
+//Math.random()
+//Math.random()* maxnum +minnum
+//+minmum because to avoid 0
+//use .floor to remove decimals
+// formula
+//Math.random()*(max-min+1)+min
+ // max-min+1 is range
