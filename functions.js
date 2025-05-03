92c622d979fc442271fd7289ade8043d4486a14f
kosanaharshavardhan <kosana.harshavardhan0705@gmail.com>
2025-05-03 10:14:54 +0530

    add function

--- functions.js
+++ functions.js
@@ -1,3 +1,21 @@
 function print(){
     console.log("printing...")
 }
+print()
+//3+null ==3
+
+// default parameter
+let fun=function(para= 'defaultName'){
+    return para;
+}
+console.log(fun())
+// rest parameter unlimited arguments
+//fun(...num)
+function a(y,z,...arr){
+    return arr;
+}
+console.log(a(1,2,3,4,5))
+// function parameters can be objs
+//function({a:1}) //passing obj
+//function([1,2,3,]) //passing array
+
