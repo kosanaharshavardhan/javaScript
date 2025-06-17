3e62cace2c36abb07f9ebe4773f379aaa28d23b7
kosanaharshavardhan <kosana.harshavardhan0705@gmail.com>
2025-06-17 20:06:38 +0530

    U know guard

--- controlflow.js
+++ controlflow.js
@@ -5,3 +5,5 @@ val1= 5 ?? 10 // returns what is true
 // like guard operator 
 val2 = 5 && 2 // checks like condition and return that value when conditiom satisfies
 console.log(val1,val2)
+
+console.log(undefined ?? null)
