eb6efee8e815f18d01c9cf576658e6f47df8f35d
kosanaharshavardhan <kosana.harshavardhan0705@gmail.com>
2025-06-17 20:00:16 +0530

    lets goo

---
+++ controlflow.js
@@ -0,0 +1,7 @@
+// if else
+// NULLISH COalescing operator
+
+val1= 5 ?? 10 // returns what is true
+// like guard operator 
+val2 = 5 && 2 // checks like condition and return that value when conditiom satisfies
+console.log(val1,val2)
--- jec.js
+++ jec.js
@@ -1,4 +1,6 @@
 //Java Script execution context
 //  2 phases
 // 1. memory creation and execution phase
-// global exectuoinal context and finciton execution context
+// global exectuoinal context and funciton execution context
+// call stack
+
