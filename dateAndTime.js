280b8694c016c4289f7e1a06c2d1d0472ca9c749
kosanaharshavardhan <kosana.harshavardhan0705@gmail.com>
2025-06-18 06:15:06 +0530

    commit d & t

--- dateAndTime.js
+++ dateAndTime.js
@@ -1,19 +1,21 @@
 let myDate=new Date()
-console.log(myDate.toString())
-console.log(myDate.toDateString())
-console.log(myDate.toLocaleDateString('default',{
-    timeZone
-}))
-console.log(typeof myDate)
-//custom dates
-let cusDate=new Date(2025,4,1,6,33,23);
-console.log(cusDate.toString())
-// new Date("yyyy-mm-dd")
-//new Date("mm-dd-yyyy")
-//new Date("dd-mm-yyyy")
- // in js date.now represents no. of milliseconds passed after jan 1 1970 midnight(weird right)
- let time=Date.now();
- console.error(time)
- console.log(cusDate.getTime())//in milliseconds
- //.getMonth()
- //.getDay()
+// console.log(myDate.toString())
+// console.log(myDate.toDateString())
+// console.log(myDate.toLocaleDateString('default',{
+//     timeZone
+// }))
+// console.log(typeof myDate)
+// //custom dates
+// let cusDate=new Date(2025,4,1,6,33,23);
+// console.log(cusDate.toString())
+// // new Date("yyyy-mm-dd")
+// //new Date("mm-dd-yyyy")
+// //new Date("dd-mm-yyyy")
+//  // in js date.now represents no. of milliseconds passed after jan 1 1970 midnight(weird right)
+//  let time=Date.now();
+//  console.error(time)
+//  console.log(cusDate.getTime())//in milliseconds
+//  //.getMonth()
+//  //.getDay()
+
+console.log(myDate.getTime())
