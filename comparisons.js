7c06c6efcc89702bd98eb0a824b1522d0bd1bed6
kosanaharshavardhan <kosana.harshavardhan0705@gmail.com>
2025-06-18 05:46:37 +0530

    some updation

--- comparisons.js
+++ comparisons.js
@@ -6,3 +6,7 @@ console.log(null>=0) //true 0>=0
 console.log(undefined==0)
 console.log(undefined>=0)
 console.log(undefined<0)
+
+console.log("2" > 1);
+console.log("02" > 1);
+
--- typeConversion.js
+++ typeConversion.js
@@ -3,12 +3,16 @@ let numstr=Number(str);
 console.log(typeof numstr)
 console.log(NaN===NaN)//use .isNan()
 // Number(null)=0;
-
-
-
+console.log(numstr);
+console.error(typeof NaN)
+console.log("hi"+"how")
 //
 console.log("1"+2);
 console.log("1"+2+2)
 console.log(1+1+"2")
-
+console.log(+"")
 //+true --> a number conversion
+let a,b,c
+
+a=b=c='$r-'
+console.log(a,b,c)
