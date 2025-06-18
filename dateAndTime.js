04aab0006e18e662ccb52009e9ff3186124a4bd7
kosanaharshavardhan <kosana.harshavardhan0705@gmail.com>
2025-06-18 06:21:56 +0530

    d&t done

--- dateAndTime.js
+++ dateAndTime.js
@@ -1,6 +1,6 @@
 let myDate=new Date()
-// console.log(myDate.toString())
-// console.log(myDate.toDateString())
+console.log(myDate.toString())
+console.log(myDate.toLocaleDateString())
 // console.log(myDate.toLocaleDateString('default',{
 //     timeZone
 // }))
@@ -12,7 +12,8 @@ let myDate=new Date()
 // //new Date("mm-dd-yyyy")
 // //new Date("dd-mm-yyyy")
 //  // in js date.now represents no. of milliseconds passed after jan 1 1970 midnight(weird right)
-//  let time=Date.now();
+ let time=Date.now();
+ console.log(time)
 //  console.error(time)
 //  console.log(cusDate.getTime())//in milliseconds
 //  //.getMonth()
