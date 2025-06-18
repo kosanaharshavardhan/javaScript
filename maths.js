3874974389cf4f17bda0155ea04dae313e3178a7
kosanaharshavardhan <kosana.harshavardhan0705@gmail.com>
2025-06-18 06:05:03 +0530

    maths done

--- maths.js
+++ maths.js
@@ -13,6 +13,8 @@ console.log(num)
 //tIP: USE DATATYPE() function in console brwoser window to know properties or length
 // or when you declare a variable using a constructor method you can also view using it
 //=-=-=-=--=-=-=++_+_+_+_+_+-=+-
+
+
 //math methods
 console.log(Math.ceil(0.4));
 //Math.random()
@@ -23,3 +25,21 @@ console.log(Math.ceil(0.4));
 //Math.random()*(max-min+1)+min
 console.log(Math.floor(Math.random()*(10)+1))
  // max-min+1 is range
+
+ const otherNumber = 123.896
+
+console.log(otherNumber.toPrecision(4));
+
+
+
+// I give you array give me randome ele of array everytime i print it
+
+let arr=[1,2,3,4,5,6,7,8,9,0];
+// console.log("Enter array size as 10")
+
+// console.log("Enter elements:")
+// for(let i=0;i<10;i++)[
+//     arr.push(ele)
+// ]
+
+console.log(arr[Math.floor(Math.random()*10)])
