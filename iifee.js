b96a923ac41c7b2cd5e45e874a25beb869b390f4
kosanaharshavardhan <kosana.harshavardhan0705@gmail.com>
2025-06-19 07:06:37 +0530

    iife done

---
+++ iifee.js
@@ -0,0 +1,12 @@
+// // imediately invoked function  call
+
+
+
+// // (function(){
+
+// named iife function have name
+// no name arrow func no name
+// // })()
+
+// // wrap in brackets and call
+// call is same like normal function can pass arguments acoordingly to the function
