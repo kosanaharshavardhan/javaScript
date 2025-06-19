aeabe0505b54cb2a2cea076318f0e575b06851ed
kosanaharshavardhan <kosana.harshavardhan0705@gmail.com>
2025-06-19 07:04:21 +0530

    sparrow arrow

--- arrow.js
+++ arrow.js
@@ -7,6 +7,27 @@ function print(){
     let a=1
     console.log(this.a);
     //runs on global context
-    // this keyword cannot be used.
+    // this keyword cannot be used.(strcit mode)
 }
 print()
+
+const user = {
+    username: "hitesh",
+    price: 999,
+
+    welcomeMessage: function() {
+        console.log(`${this.username} , welcome to website`);
+        console.log(this);
+    }
+
+}
+user.welcomeMessage()
+const chai =  () => {
+    let username = "hitesh"
+    console.log(this);
+}
+chai()
+
+// (a,b,)=>(a+b)
+// ()--> t0 return wwithout return keyword
+
