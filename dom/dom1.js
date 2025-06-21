fa9c7f5b07eb0c2a6aaab4a726b6d34030aa2d9b
kosanaharshavardhan <kosana.harshavardhan0705@gmail.com>
2025-06-21 07:07:35 +0530

    here comes query selector

--- dom/dom1.js
+++ dom/dom1.js
@@ -42,3 +42,56 @@
 
 // a.style.backgroundColor='red'
 // 'red'
+
+
+
+// h1.textContent
+
+// let h1=document.getElementById('hi')
+// undefined
+
+
+// h1.innerText
+// 'hiiiii'
+// it ignores the invisible values even though it present on html code
+
+
+// h1.textContent
+// 'hiiiiiInvisible text'
+// see it gave invisible value also
+
+
+
+// h1.innerHTML
+// 'hiiiii<span style="display: none;">Invisible text</span>'
+// it gives you a full html code
+
+
+
+//document.querySelector('h1')
+//<h1 style=​"color:​ white;​" id=​"hi" class=​"hi1">​…​</h1>​
+//querselector('.class')
+// selets only first ele if multiple ele are there
+// selection can be useda s css selectors
+
+
+// let ul=document.querySelector('ul')
+// undefined
+
+
+// ul
+// <ul>​…​</ul>​
+
+
+// ul.querySelector('li')
+// <li>​::marker​"1"</li>​
+
+
+// let li=ul.querySelector('li')
+// undefined
+
+
+// li.style.color='red'
+//'red'
+
+// can manipulate easily
