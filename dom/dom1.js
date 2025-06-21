4efa2109669187790c9ba67705334e3be8725d54
kosanaharshavardhan <kosana.harshavardhan0705@gmail.com>
2025-06-21 06:48:07 +0530

    cooking the dom dish

---
+++ dom/dom1.js
@@ -0,0 +1,44 @@
+// doc
+
+// document.getElementById('hi')
+
+// <h1 style=​"color:​ white;​" id=​"hi" class=​"hi1">​hiiiii​</h1>​
+
+// document.getElementById('hi').id
+// 'hi'
+
+// document.getElementById('hi').className
+// 'hi1'
+
+// document.getElementById('hi').className.innerHTML
+// undefined
+
+
+// document.getElementById('hi').className.innerHTML()
+
+
+
+
+// document.getElementById('hi').getAttribute(class)
+
+
+// document.getElementById('hi').getAttribute('class')
+// 'hi1'
+
+
+
+// document.getElementById('hi').setAttribute('class', 'test hi')
+// undefined
+
+// console.log('don;t override the previous class')
+
+
+
+// let a= document.getElementById('hi')
+// undefined
+
+
+
+
+// a.style.backgroundColor='red'
+// 'red'
