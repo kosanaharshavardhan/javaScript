8aeca278a0af0259192fce221a8c502fc5a4f823
kosanaharshavardhan <kosana.harshavardhan0705@gmail.com>
2025-12-30 22:44:27 +0530

    Update dom1.js

--- dom/dom1.js
+++ dom/dom1.js
@@ -1,97 +1,97 @@
-// doc
+doc
 
-// document.getElementById('hi')
+document.getElementById('hi')
 
-// <h1 style=​"color:​ white;​" id=​"hi" class=​"hi1">​hiiiii​</h1>​
+<h1 style=​"color:​ white;​" id=​"hi" class=​"hi1">​hiiiii​</h1>​
 
-// document.getElementById('hi').id
-// 'hi'
+document.getElementById('hi').id
+'hi'
 
-// document.getElementById('hi').className
-// 'hi1'
+document.getElementById('hi').className
+'hi1'
 
-// document.getElementById('hi').className.innerHTML
-// undefined
-
-
-// document.getElementById('hi').className.innerHTML()
+document.getElementById('hi').className.innerHTML
+undefined
 
 
-
-
-// document.getElementById('hi').getAttribute(class)
-
-
-// document.getElementById('hi').getAttribute('class')
-// 'hi1'
-
-
-
-// document.getElementById('hi').setAttribute('class', 'test hi')
-// undefined
-
-// console.log('don;t override the previous class')
-
-
-
-// let a= document.getElementById('hi')
-// undefined
+document.getElementById('hi').className.innerHTML()
 
 
 
 
-// a.style.backgroundColor='red'
-// 'red'
+document.getElementById('hi').getAttribute(class)
+
+
+document.getElementById('hi').getAttribute('class')
+'hi1'
 
 
 
-// h1.textContent
+document.getElementById('hi').setAttribute('class', 'test hi')
+undefined
 
-// let h1=document.getElementById('hi')
-// undefined
-
-
-// h1.innerText
-// 'hiiiii'
-// it ignores the invisible values even though it present on html code
-
-
-// h1.textContent
-// 'hiiiiiInvisible text'
-// see it gave invisible value also
+console.log('don;t override the previous class')
 
 
 
-// h1.innerHTML
-// 'hiiiii<span style="display: none;">Invisible text</span>'
-// it gives you a full html code
+let a= document.getElementById('hi')
+undefined
 
 
 
-//document.querySelector('h1')
-//<h1 style=​"color:​ white;​" id=​"hi" class=​"hi1">​…​</h1>​
-//querselector('.class')
-// selets only first ele if multiple ele are there
-// selection can be useda s css selectors
+
+a.style.backgroundColor='red'
+'red'
 
 
-// let ul=document.querySelector('ul')
-// undefined
+
+h1.textContent
+
+let h1=document.getElementById('hi')
+undefined
 
 
-// ul
-// <ul>​…​</ul>​
+h1.innerText
+'hiiiii'
+it ignores the invisible values even though it present on html code
 
 
-// ul.querySelector('li')
-// <li>​::marker​"1"</li>​
+h1.textContent
+'hiiiiiInvisible text'
+see it gave invisible value also
 
 
-// let li=ul.querySelector('li')
-// undefined
+
+h1.innerHTML
+'hiiiii<span style="display: none;">Invisible text</span>'
+it gives you a full html code
 
 
-// li.style.color='red'
-//'red'
 
-// can manipulate easilyy
+document.querySelector('h1')
+<h1 style=​"color:​ white;​" id=​"hi" class=​"hi1">​…​</h1>​
+querselector('.class')
+selets only first ele if multiple ele are there
+selection can be useda s css selectors
+
+
+let ul=document.querySelector('ul')
+undefined
+
+
+ul
+<ul>​…​</ul>​
+
+
+ul.querySelector('li')
+<li>​::marker​"1"</li>​
+
+
+let li=ul.querySelector('li')
+undefined
+
+
+li.style.color='red'
+'red'
+
+can manipulate easilyy
